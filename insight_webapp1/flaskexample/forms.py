f7e89840276654c43be90b33160c9a6593abfd2c
yanfeiw2018 <yanfeiw.2013@gmail.com>
2019-06-30 20:01:50 -0400

    folder name change

--- insight_webapp1/flaskexample/forms.py
+++
@@ -1,6 +0,0 @@
-from wtforms import Form, StringField, SelectField
- 
-class SearchForm(Form):
-    choices = [('Keyword', 'Keyword')]
-    select = SelectField( choices=choices)
-    search = StringField('')
